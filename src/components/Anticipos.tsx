7e5219464546c707c2318f9c67e287f57fe50dd7
jldev1227 <1227jldev@gmail.com>
2024-10-02 15:17:05 -0500

    [FEATURE #11] Implementar tabs en la liquidación
    - Se agrego navegación mediante tabs para poder consultar la liquidación realizada y adicional los anticipos descontados al conductor en la liquidación.
    - Se pueden agregar anticipos y eliminar anticipos.
    
    Closes #45

--- src/components/Anticipos.tsx
+++ src/components/Anticipos.tsx
@@ -2,25 +2,16 @@ import { formatCurrency, formatToCOP } from "@/helpers";
 import useLiquidacion from "@/hooks/useLiquidacion";
 import { Anticipo } from "@/types";
 import { Input } from "@nextui-org/input";
+import { Tooltip } from "@nextui-org/tooltip";
 import { Button } from "@nextui-org/button";
 import { FormEvent, useState } from "react";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableColumn,
-  TableHeader,
-  TableRow,
-} from "@nextui-org/table";
 
 export default function Anticipos() {
-  const { state } = useLiquidacion();
+  const { state, agregarAnticipos, eliminarAnticipo } = useLiquidacion();
   const [anticipos, setAnticipos] = useState<Anticipo[]>([]);
   const [errorMessages, setErrorMessages] = useState<string[]>([]);
 
   const { liquidacion } = state;
-
-  console.log(liquidacion?.anticipos?.map((anticipo) => anticipo.valor));
 
   // Maneja el cambio en el input
   const handleAnticipoChange = (
@@ -51,7 +42,7 @@ export default function Anticipos() {
     });
   };
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const newErrorMessages: string[] = [];
@@ -74,8 +65,9 @@ export default function Anticipos() {
       return; // No continuar si hay errores
     }
 
-    // Aquí puedes continuar con el procesamiento del formulario
-    console.log("Anticipos válidos:", anticipos);
+    await agregarAnticipos(anticipos);
+  
+    setAnticipos([])
   };
 
   return (
@@ -91,14 +83,11 @@ export default function Anticipos() {
               + Añadir anticipo
             </Button>
             {anticipos.map((anticipo, index) => (
-              <div>
-                <div
-                  key={index}
-                  className="flex justify-between items-center gap-5"
-                >
+              <div key={index}>
+                <div className="flex justify-between items-center gap-5">
                   <Input
                     type="text"
-                    label={`Anticipo ${index + 1}`}
+                    label={"Valor"}
                     placeholder="$0"
                     className="max-w-md"
                     value={
@@ -137,14 +126,14 @@ export default function Anticipos() {
               Anticipos realizados
             </h2>
             {Array.isArray(liquidacion?.anticipos) &&
-          liquidacion.anticipos.length > 0 ? (
-   
+            liquidacion.anticipos.length > 0 ? (
               <div>
                 <table className="table-auto w-full text-sm mb-5">
                   <thead className="bg-red-500 text-white">
                     <tr>
                       <th className="px-4 py-2 text-left">#</th>
                       <th className="px-4 py-2 text-center">Valor</th>
+                      <th className="px-4 py-2 text-center">Acciones</th>
                     </tr>
                   </thead>
                   <tbody>
@@ -154,16 +143,57 @@ export default function Anticipos() {
                         <td className="border px-4 py-2 text-center">
                           {formatToCOP(anticipo.valor)}
                         </td>
+                        <td className="border px-4 py-2 text-center items-center space-x-2">
+                          <Tooltip content="Editar" color="primary">
+                            <Button color="primary" className="h-9" isIconOnly>
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="size-5"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                                />
+                              </svg>
+                            </Button>
+                          </Tooltip>
+                          <Tooltip content="Eliminar" color="danger">
+                            <Button
+                              onPress={()=>eliminarAnticipo(anticipo.id, state.liquidacion?.id)}
+                              color="danger"
+                              className="h-9"
+                              isIconOnly
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="size-5"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                                />
+                              </svg>
+                            </Button>
+                          </Tooltip>
+                        </td>
                       </tr>
                     ))}
                   </tbody>
                 </table>
               </div>
-          ) : (
-            <h2 className="flex-1 text-xl lg:text-2xl">
-              No hay anticipos realizados
-            </h2>
-          )}
+            ) : (
+              <h2 className="flex-1 text-md">No hay anticipos realizados</h2>
+            )}
           </div>
         </div>
       )}
